12acac1f595b2ed8afb69f42f68dd57f7388727d
prettylolly <you@exampletaaibatbasaru@gmail.com>
2022-09-08 15:47:35 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entrypoint
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	getcher();
+	return (0);
+}
