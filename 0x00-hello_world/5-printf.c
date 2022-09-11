df68602bac11ea68df9efe80e6eb1516a17d68e1
prettylolly <you@exampletaaibatbasaru@gmail.com>
2022-09-11 04:10:56 -0700

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- *
- *  *main - Entry point
+ *main - Entry point
  *
  *   *
  *
  *    *Return: Always 0 (Success)
  *
- *     */
+ */
 
 int main(void)
 
@@ -19,5 +18,4 @@ int main(void)
 
 
 			return (0);
-
 }
