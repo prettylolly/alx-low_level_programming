04f2b4c49ec49c2067da379c9cf981c0d3b1a4d9
prettylolly <you@exampletaaibatbasaru@gmail.com>
2022-09-12 11:37:02 -0700

    Numberz

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -24,12 +24,14 @@ int main(void)
 				putchar((j % 10) + 48);
 				if (i != 98 || j != 99)
 				{
-					putchar(',');
-					putchar(' ');
-	 			}
+					putchar(',')
+					putchar(' ')
+				}
 			}
-	      }
+		}
 	}
+
+	
 	putchar('\n');
 	return (0);
 }
