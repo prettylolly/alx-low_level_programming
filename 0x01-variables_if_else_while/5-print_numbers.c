0b8a7d5a8380749694e27521a566c722609317c9
prettylolly <you@exampletaaibatbasaru@gmail.com>
2022-09-12 10:48:53 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print 0-9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (o):
+}
