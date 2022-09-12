e598eeba0a24f2aa3725667010f1f9f3016d6fe4
prettylolly <you@exampletaaibatbasaru@gmail.com>
2022-09-12 14:04:34 -0700

    mile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include ,stdio.h>
+
+/**
+ * main imain block
+ *
+ * Description: prints in alphabets in reverse
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch)
+		putchar('\n')
+		return (0);
+}
