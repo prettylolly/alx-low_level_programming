02d59d7998293858e89cf238ad38813d77095603
prettylolly <you@exampletaaibatbasaru@gmail.com>
2022-09-12 11:02:45 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints 0-9
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+        printf("%d", n);
+	printf("\n");
+	return (0);
+}
+
