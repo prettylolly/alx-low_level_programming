41650fc6052e9cbd9dd324082dd4e4cf5c95fad0
prettylolly <you@exampletaaibatbasaru@gmail.com>
2022-09-14 07:47:29 -0700

    I sometimes suffer from insomni

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,19 +2,19 @@
 
 /**
  * print_alphabet - Entry point
- * Return:0
+ * Description: a function that prints the alphabet,
+ * in lowcase followed by a new line
+ * Return: void
  */
-
 void print_alphabet(void)
 {
 	char c = 'a';
 
 	while (c <= 'z')
 	{
-		_putchar(c);
-		c++;
+		_putchar(letter);
+		letter++;
 
 	}
 	_putchar('\n');
-
 }
