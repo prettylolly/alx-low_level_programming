9e91c9f133e2447cc443dbf304615dfe4f8110b3
prettylolly <you@exampletaaibatbasaru@gmail.com>
2022-09-14 16:59:14 -0700

    There are only 3 colors

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value
+ * of an integer.
+ *
+ * @i: input number either positive or negative
+ *
+ * Return: absolute value
+ */
+int _abs(int i)
+{
+	f (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
