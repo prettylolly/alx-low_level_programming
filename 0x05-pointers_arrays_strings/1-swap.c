b0372cddf6400608c683ce65ea03b08bb7954a8c
prettylolly <you@exampletaaibatbasaru@gmail.com>
2022-09-19 06:49:40 -0700

    . Great leaders are willing to sacrifice the numbers to save the people Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,20 +1,16 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
+ * swap_int - functons that swaps the values of two integers.
+ * @a: the integer adresse
+ * @b: the integer adresse
  *
- * Return: Always 0.
+ * Return: 1 or 0
  */
-int main(void)+
-{
-	int a;
-	int b;
 
-	a = 98;
-	b = 42;
-	printf("a=%d, b=%d\n", a, b);
-	swap_int(&a, &b);
-	printf("a=%d, b=%d\n", a, b);
-	return (0);
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+
+	*a = *b;
+	*b = c;
 }
