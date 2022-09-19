050b66a110b51e425e516aefa94160e33215b371
prettylolly <you@exampletaaibatbasaru@gmail.com>
2022-09-19 09:26:30 -0700

    This report by its very length defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,7 @@
 /**
  * _strlen - a function that takes a pointer to an int as parameter and
  *pdates the value it points to to 98
- *@s: chaine of caractere
+ * @s: chaine of caractere
  *
  * Return: 1 or 0
  */
@@ -12,7 +12,7 @@ int _strlen(char *s)
 {
 	int i = 0;
 
-	hile (s[i])
-		i++;
+	while (s[i])
+	        i++;
 	return (i);
 }
