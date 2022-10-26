214e68d5f97143ee91cdd412f634092ce2b24a9d
prettylolly <you@exampletaaibatbasaru@gmail.com>
2022-10-26 09:57:30 -0700

    list lenght

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,7 +3,7 @@
 
 /**
  * listint_len - Returns the number of elements
- * 		 in a linked listint_t list.
+ *               in a linked listint_t list.
  * @h: A pointer to the head of the listint_t list.
  *
  * Return: The number of elements in the listint_t list.
