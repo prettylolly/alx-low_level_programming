a134f2bae27a87e1bb091d60ef7ffa74b666c4c1
prettylolly <you@exampletaaibatbasaru@gmail.com>
2022-10-26 10:08:48 -0700

    list lenght

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,8 +2,7 @@
 #include <stdio.h>
 
 /**
- * listint_len - Returns the number of elements
- *               in a linked listint_t list.
+ * listint_len - Returns the number of elements of a listint_t list.
  * @h: A pointer to the head of the listint_t list.
  *
  * Return: The number of elements in the listint_t list.
