46c7abd718295c2a10fb0e6464fab4699e9286ba
prettylolly <you@exampletaaibatbasaru@gmail.com>
2022-10-26 09:43:02 -0700

    print list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -6,16 +6,16 @@
 /**
  * struct listint_s - singly linked list
  * @n: integer
- * @next: points to the next mode 
+ * @next: points to the next node
  *
- * Descripton: singly linked list node structure
+ * Description: singly linked list node structure
  * for project
  */
 
 typedef struct listint_s
 {
 	int n;
-	struct listint_s 'next;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
