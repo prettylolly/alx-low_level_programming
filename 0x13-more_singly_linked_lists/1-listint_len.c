911c5aabe7bcd6bd4a6da80250239dcb6555d19c
prettylolly <you@exampletaaibatbasaru@gmail.com>
2022-10-26 10:17:06 -0700

    list lenght

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,13 +7,14 @@
  *
  * Return: The number of elements in the listint_t list.
  */
-size_t listint_len(const listint_t *h)
+size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
 	
 	while (h)
 	{
 		nodes++;
+		printf{"%d/n",h->n};
 		h = h->next;
 	}
 
---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+
+
+/**
+ *
+ *  * add_nodeint - Adds a new node at the beginning
+ *
+ *   *               of a listint_t list.
+ *
+ *    * @head: A pointer to the address of the
+ *
+ *     *        head of the listint_t list.
+ *
+ *      * @n: The integer for the new node to contain.
+ *
+ *       *
+ *
+ *        * Return: If the function fails - NULL.
+ *
+ *         *         Otherwise - the address of the new element.
+ *
+ */
