4e682074177231d78cb7cbec6a422e6f3f3fb8a3
prettylolly <you@exampletaaibatbasaru@gmail.com>
2022-10-28 06:12:40 -0700

    0 task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,13 +1,23 @@
 #include "main.h"
 
 /**
- * print_binary - Prints the binary representation of a number.
- * @n: The number to be printed in binary.
- *    */
-void print_binary(unsigned long int n)
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
 {
-		if (n > 1)
-			print_binary(n >> 1);
+	int i;
+	unsigned int dec_val = 0;
 
-			_putchar((n & 1) + '0');
+	if (!b)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
+	}
+	return (dec_val);
 }
